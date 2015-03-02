b9d925f4e6ed6a2bfd4e8a6ee455515197678292
Sahil Kapoor <sahilkapoor1109@gmail.com>
2015-03-02 14:28:59 +0530

    Created Xcode Project

---
+++ HCSParallaxCarousel/HCSParallaxCarousel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HCSParallaxCarousel
+//
+//  Created by Sahil Kapoor on 02/03/15.
+//  Copyright (c) 2015 Hot Cocoa Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
